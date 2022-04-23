feb0bb4ca0ee9cc6c61c0b9a7007f0b762c818de
zench0 <aksjonov_is@edu.surgu.ru>
2022-04-23 21:18:27 +0500

    бд

--- howmany_db.sql
+++ howmany_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Апр 02 2022 г., 17:26
+-- Время создания: Апр 06 2022 г., 09:49
 -- Версия сервера: 8.0.24
 -- Версия PHP: 7.1.33
 
@@ -29,11 +29,22 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `events` (
   `ID` int NOT NULL COMMENT 'ID конференции',
-  `Event_name` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'Название События',
-  `Date` date NOT NULL COMMENT 'Дата',
-  `Time` time NOT NULL COMMENT 'Время',
-  `Event_Commentary` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'Комментарий к событию'
+  `name` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'Название События',
+  `date_time` datetime NOT NULL COMMENT 'Дата',
+  `сommentary` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'Комментарий к событию'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='События, в которых могут находиться участники ';
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `events_members`
+--
+
+CREATE TABLE `events_members` (
+  `ID` int NOT NULL,
+  `ID_Event` int NOT NULL,
+  `ID_Member` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
@@ -42,9 +53,10 @@ CREATE TABLE `events` (
 --
 
 CREATE TABLE `members` (
-  `ID_member` int NOT NULL COMMENT 'ID Участника',
-  `ID_Event` int DEFAULT NULL COMMENT 'ID События',
-  `member_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'ФИО участника'
+  `ID` int NOT NULL COMMENT 'ID Участника',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'ФИО участника',
+  `login` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'login участника',
+  `password` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'пароль участника'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Таблица со списком участником';
 
 --
@@ -58,11 +70,18 @@ ALTER TABLE `events`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Индексы таблицы `events_members`
+--
+ALTER TABLE `events_members`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `ID_Event` (`ID_Event`),
+  ADD KEY `ID_Member` (`ID_Member`);
+
+--
 -- Индексы таблицы `members`
 --
 ALTER TABLE `members`
-  ADD PRIMARY KEY (`ID_member`),
-  ADD KEY `ID_Event` (`ID_Event`);
+  ADD PRIMARY KEY (`ID`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
@@ -75,20 +94,27 @@ ALTER TABLE `events`
   MODIFY `ID` int NOT NULL AUTO_INCREMENT COMMENT 'ID конференции';
 
 --
+-- AUTO_INCREMENT для таблицы `events_members`
+--
+ALTER TABLE `events_members`
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT для таблицы `members`
 --
 ALTER TABLE `members`
-  MODIFY `ID_member` int NOT NULL AUTO_INCREMENT COMMENT 'ID Участника';
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT COMMENT 'ID Участника';
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
 --
 
 --
--- Ограничения внешнего ключа таблицы `members`
+-- Ограничения внешнего ключа таблицы `events_members`
 --
-ALTER TABLE `members`
-  ADD CONSTRAINT `members_ibfk_1` FOREIGN KEY (`ID_Event`) REFERENCES `events` (`ID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `events_members`
+  ADD CONSTRAINT `events_members_ibfk_1` FOREIGN KEY (`ID_Event`) REFERENCES `events` (`ID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `events_members_ibfk_2` FOREIGN KEY (`ID_Member`) REFERENCES `members` (`ID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
